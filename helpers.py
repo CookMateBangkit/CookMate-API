e2bb3e1f1632e1cd0cc89aeee823c564391632b2
adanngrha <contact.ardana@gmail.com>
2023-06-07 16:34:50 +0700

    fix null error after first prediction

--- helpers.py
+++ helpers.py
@@ -8,31 +8,28 @@ model = None
 
 def load_model():
   model = tf.keras.models.load_model('./model_capstone.h5')
-  print("Model loaded")
   return model
 
-def predict(image: Image.Image):
+def predict_image(image: Image.Image):
   global model
   if model is None:
     model = load_model()
     
-    image = np.asarray(image.resize((300, 300)))[..., :3]
-    print(image.shape)
-    image = np.expand_dims(image, 0)
-    print(image.shape)
-    image = image / 127.5 - 1.0
-    
-    class_probabilities = model.predict(image) 
+  image = np.asarray(image.resize((300, 300)))[..., :3]
+  image = np.expand_dims(image, 0)
+  image = image / 127.5 - 1.0
+  
+  class_probabilities = model.predict(image) 
 
-    class_names = ['ayam', 'brokoli', 'jagung', 'kacang_tanah', 'kangkung', 'kentang', 'labu', 'labu_siam', 'lobak_merah', 'mentimun', 'nanas', 'nangka', 'nasi_putih', 'paprika', 'pare', 'pepaya', 'pisang', 'singkong', 'tahu', 'telur', 'tempe', 'terong', 'tomat', 'ubi', 'wortel']
-    predicted_classes_index = np.argmax(class_probabilities)
-    predicted = class_names[predicted_classes_index]
+  class_names = ['ayam', 'brokoli', 'jagung', 'kacang_tanah', 'kangkung', 'kentang', 'labu', 'labu_siam', 'lobak_merah', 'mentimun', 'nanas', 'nangka', 'nasi_putih', 'paprika', 'pare', 'pepaya', 'pisang', 'singkong', 'tahu', 'telur', 'tempe', 'terong', 'tomat', 'ubi', 'wortel']
+  predicted_classes_index = np.argmax(class_probabilities)
+  predicted = class_names[predicted_classes_index]
 
-    result = {
-      "predicted": predicted
-    }
+  result = {
+    "predicted": predicted
+  }
         
-    return result
+  return result
     
 def read_imagefile(file) -> Image.Image:
   image = Image.open(BytesIO(file))
--- main.py
+++ main.py
@@ -2,21 +2,21 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 import uvicorn
 
-from helpers import predict, read_imagefile
+from helpers import predict_image, read_imagefile
 
 app = FastAPI(title='CookMate!')
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
-@app.post("/api/predict")
+@app.post("/api/predict", status_code=200)
 async def predict_api(file: UploadFile = File(...)):
     extension = file.filename.split(".")[-1] in ("jpg","jpeg","png")
     if not extension:
         return "Image must be jpg or png format!"
     image = read_imagefile(await file.read())
-    prediction = predict(image)
+    prediction = predict_image(image)
     headers = {"Content-Type" : "undefined"}
     return JSONResponse(content=prediction, headers=headers)
 
