81a7d7132eb6a1184339018aef7fe1cabcdde974
adanngrha <contact.ardana@gmail.com>
2023-06-09 16:25:48 +0700

    simplified progress of predicting image

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 import uvicorn
+import requests
 
 from helpers import predict_image, read_imagefile
 
@@ -16,9 +17,12 @@ async def predict_api(file: UploadFile = File(...)):
     if not extension:
         return "Image must be jpg or png format!"
     image = read_imagefile(await file.read())
-    prediction = predict_image(image)
-    headers = {"Content-Type" : "undefined"}
-    return JSONResponse(content=prediction, headers=headers)
+    predict_image(image)
+    
+    url = "https://backend-g7swsp3jxa-as.a.run.app/resep"
+    recipes = requests.get(url)
+    
+    return {"data": recipes.json()}
 
 if __name__ == "__main__":
     uvicorn.run(app, debug=True)
