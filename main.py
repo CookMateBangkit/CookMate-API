91996d0647cf191866a0404466c4e4d3b576a17d
adanngrha <contact.ardana@gmail.com>
2023-06-10 12:52:16 +0700

    add /ingredients endpoint to return all ingredient

--- main.py
+++ main.py
@@ -2,24 +2,13 @@ from fastapi import FastAPI, UploadFile, File
 import uvicorn
 
 from classifier_helpers import predict_image, read_imagefile
-from recipe_helpers import get_recommendation, get_all_recipes, get_detail_recipe
+from recipe_helpers import get_recommendation, get_all_recipes, get_detail_recipe, get_all_ingredients
 
 app = FastAPI(title='CookMate!')
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
-@app.post("/predict", status_code=200)
-async def predict_api(file: UploadFile = File(...)):
-    extension = file.filename.split(".")[-1] in ("jpg","jpeg","png")
-    if not extension:
-        return "Image must be jpg or png format!"
-    image = read_imagefile(await file.read())
-    prediction = predict_image(image)
-    recommendation_result = get_recommendation(prediction)
-    
-    return {"data": recommendation_result}
 
 @app.get("/recipe", status_code=200)
 async def all_recipe():
@@ -34,6 +23,34 @@ async def detail_recipe(id: str):
     recipe = get_detail_recipe(id)
     
     return {"data": recipe}
+
+@app.get("/ingredients", status_code=200)
+async def all_ingredients():
+    
+    ingredients = get_all_ingredients()
+    
+    return {"data": ingredients}
+
+@app.post("/predict", status_code=200)
+async def predict_api(file: UploadFile = File(...)):
+    extension = file.filename.split(".")[-1] in ("jpg","jpeg","png")
+    if not extension:
+        return "Image must be jpg or png format!"
+    image = read_imagefile(await file.read())
+    prediction = predict_image(image)
+    recommendation_result = get_recommendation(prediction)
+    
+    return {"data": recommendation_result}
+
+@app.post("/predict-text", status_code=200)
+async def predict_text(text: str):
+    
+    # jadikan sebuah list
+    # text = list
+    
+    recommendation_result = get_recommendation(text)
+    
+    return {"data": recommendation_result}
 
 if __name__ == "__main__":
     uvicorn.run(app, debug=True)
--- recipe_helpers.py
+++ recipe_helpers.py
@@ -2,27 +2,29 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-dataset = None
+dataset_recipes = None
+dataset_ingredients = None
 
 def load_dataset():
-  dataset = pd.read_excel('./dataset.xlsx')
-  
-  return dataset
+  dataset_recipes = pd.read_excel('./dataset_recipes.xlsx')
+  dataset_ingredients = pd.read_excel('./dataset_ingredients.xlsx')
+  return dataset_recipes, dataset_ingredients
 
 def get_recommendation(keyword):
   
-  global dataset
-  if dataset is None:
-    dataset = load_dataset()
+  global dataset_recipes
+  global dataset_ingredients
+  if dataset_recipes is None or dataset_ingredients is None: 
+    dataset_recipes, dataset_ingredients = load_dataset()
     
   vectorizer = TfidfVectorizer()
-  recipe_matrix = vectorizer.fit_transform(dataset['main_ingredient'])
+  recipe_matrix = vectorizer.fit_transform(dataset_recipes['main_ingredient'])
   
   query_vector = vectorizer.transform([keyword])
   similarity_scores = cosine_similarity(query_vector, recipe_matrix)
   
   top_recipe_indices = similarity_scores.argsort()[0][::-1]
-  top_10_recipes = dataset.iloc[top_recipe_indices][:10]
+  top_10_recipes = dataset_recipes.iloc[top_recipe_indices][:10]
   recipes = top_10_recipes.reset_index().to_dict(orient='index')
   
   recipes_cleaned = list(recipes.values())
@@ -33,13 +35,29 @@ def get_recommendation(keyword):
   
   return recipes_cleaned
 
+def get_all_ingredients():
+  
+  global dataset_recipes
+  global dataset_ingredients
+  if dataset_recipes is None or dataset_ingredients is None: 
+    dataset_recipes, dataset_ingredients = load_dataset()
+  
+  ingredients = dataset_ingredients.reset_index().to_dict(orient='index')
+  ingredients = list(ingredients.values())
+  
+  for dictionary in ingredients:
+    del dictionary['index']
+  
+  return ingredients
+
 def get_all_recipes():
   
-  global dataset
-  if dataset is None:
-    dataset = load_dataset()
+  global dataset_recipes
+  global dataset_ingredients
+  if dataset_recipes is None or dataset_ingredients is None: 
+    dataset_recipes, dataset_ingredients = load_dataset()
     
-  recipes = dataset.reset_index().to_dict(orient='index')
+  recipes = dataset_recipes.reset_index().to_dict(orient='index')
   recipes = list(recipes.values())
   
   for dictionary in recipes:
@@ -50,11 +68,12 @@ def get_all_recipes():
     
 def get_detail_recipe(id):
 
-  global dataset
-  if dataset is None:
-    dataset = load_dataset()
+  global dataset_recipes
+  global dataset_ingredients
+  if dataset_recipes is None or dataset_ingredients is None: 
+    dataset_recipes, dataset_ingredients = load_dataset()
   
-  recipe = dataset.loc[dataset['id'] == id]
+  recipe = dataset_recipes.loc[dataset_recipes['id'] == id]
   recipe = recipe.to_dict('records')[0]
     
   return recipe
